e74cc9fc7efeb304c79d98aa02e7f6f5347c6c3c
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-01-13 19:32:52 +0100

    Update
    Update

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,6 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+Vue.use(VueAxios, axios)
